cc03b8e4638c53e66c67698e6747fc1d811b6ed3
Emir Alp Esmergül <emiralpce35@gmail.com>
2024-12-19 02:08:03 +0300

    .

---
+++ MyPortfolio_MVC/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using MyPortfolio_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio_MVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        MyPortfolioEntities db = new MyPortfolioEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult DefaultBanner()
+        {
+            var values = db.TblBanners.Where(x => x.IsShown == true).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultExpertise()
+        {
+            var values = db.TblExpertises.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultExperience()
+        {
+            var values = db.TblExperiences.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultAbout()
+        {
+            var values = db.TblAbouts.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult DefaultEducation()
+        {
+            var values = db.TblEducations.ToList();
+            return PartialView(values); 
+        }
+    }
+}
